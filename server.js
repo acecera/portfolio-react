f440277bf1c94b5fa90a5d3fb4ff391920acf935
Anthony Cera <ace_cera@yahoo.com>
2018-08-02 13:38:07 -0700

    corrected spelling on portfolio.

--- server.js
+++ server.js
@@ -6,5 +6,5 @@ const PORT = process.env.PORT || 5150;
 app.use(express.static('react-portfolio'));
 
 app.listen(PORT, function() {
-    console.log("Portfoliio running on port ${PORT}!");
+    console.log("Portfolio running on port ${PORT}!");
 });
