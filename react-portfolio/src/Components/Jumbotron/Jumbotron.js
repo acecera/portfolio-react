d2bef11ebf2d34ddfe470e6383bfdbaf99ff1fba
Anthony Cera <ace_cera@yahoo.com>
2018-08-03 23:20:35 -0700

    added syntax for jumbotron component and image.

--- react-portfolio/src/Components/Jumbotron/Jumbotron.js
+++ react-portfolio/src/Components/Jumbotron/Jumbotron.js
@@ -1,2 +1,17 @@
 import React from "react";
+import mountainImage from "../../Images/Mountain.png";
+import classes from "./Jumbotron.css";
+
+const Jumbotron = (props) => {
+    return (
+        <div className={classes.Jumbotron}>
+            <img 
+            className={classes.Image} 
+            src={mountainImage} 
+            alt="Mountain Resort" />;
+        </div>
+    );
+}
+
+export default Jumbotron;
 
