7d43dca58e8a9ab23ff38b4d442fb49e16b920a0
Anthony Cera <ace_cera@yahoo.com>
2018-08-03 23:21:30 -0700

    aded syntax for navbar component.

--- react-portfolio/src/Components/Navbar/Navbar.js
+++ react-portfolio/src/Components/Navbar/Navbar.js
@@ -1,12 +1,15 @@
 import React from "react";
-import {Link} from "react-router-dom";
+import {BroswerRouter, Route, Link} from "react-router-dom";
+import {Home} from "../../Pages/Home /Home"
+import {Projects} from "../../Pages/Projects/Projects";
+import {Contact }from "../../Pages/Contact/Contact";
 
 const Navbar = (props) => {
     return (
         <div className="navbar">
-            <Link className="link" to="/">Home</Link>
-            <Link className="link" to="/projects">Projects</Link>
-            <Link className="link" to="/contact">Contact</Link>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/projects" component={Projects} />
+            <Route exact path="/contact" component={Contact} /> 
         </div>
     );
 }
