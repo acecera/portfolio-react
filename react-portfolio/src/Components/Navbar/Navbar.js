6880744608463ea6e306479cac90aeefa58d12c2
Anthony Cera <ace_cera@yahoo.com>
2018-08-06 10:42:05 -0700

    added syntax for bootstrap navbar component.

--- react-portfolio/src/Components/Navbar/Navbar.js
+++ react-portfolio/src/Components/Navbar/Navbar.js
@@ -6,11 +6,27 @@ import {Contact }from "../../Pages/Contact/Contact";
 
 const Navbar = (props) => {
     return (
-        <div className="navbar">
-            <Route exact path="/" component={Home} />
-            <Route exact path="/projects" component={Projects} />
-            <Route exact path="/contact" component={Contact} /> 
-        </div>
+        <Navbar inverse collapseOnSelect>
+            <Navbar.Header>
+                <Navbar.Brand>
+                    <a href="#brand">Anthony Cera</a>
+                </Navbar.Brand>
+                <Navbar.Toggle />
+                <Navbar.Collapse>
+                    <Nav pullRight>
+                        <NavItem eventKey={1} href="/">
+                            About
+                        </NavItem>
+                        <NavItem eventKey={2} href="/projects">
+                            Projects
+                        </NavItem>
+                        <NavItem eventKey={3} href="/contact">
+                            Contact
+                        </NavItem>
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar.Header>
+        </Navbar>
     );
 }
 
