1140cb01320da1aa00f156a85afa0202d5259372
Anthony Cera <ace_cera@yahoo.com>
2018-08-07 11:56:45 -0700

    got rid of folders and put all the page files in the page folder.

---
+++ react-portfolio/src/Components/Pages/Home.jsx
@@ -0,0 +1,17 @@
+import React, {Component} from "react";
+
+import Navbar from "../../Navbar/MyNavbar";
+
+
+ export class About extends Component { 
+    render() {
+        return (
+            <div className="home">
+                <Navbar />
+               
+            </div>
+        );
+    }
+}
+
+export default About;
