b74e5333c79b1d89e003805abbfed7bcc1176c95
Anthony Cera <ace_cera@yahoo.com>
2018-08-07 11:44:23 -0700

    imported router and route, added those elements to the render function.

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import {BrowserRouter as Router, Route} from "react-router-dom";
 import './App.css';
 import Jumbotron from "./Components/Jumbotron/Jumbotron";
 import Navbar from './Components/Navbar/MyNavbar';
@@ -8,10 +8,13 @@ import Navbar from './Components/Navbar/MyNavbar';
 class App extends Component {
   render() {
     return (
-      <div className="App">
-          {/* <Navbar /> */}
-          <Jumbotron />
-      </div>
+      <Router>
+         <div className="App">
+            <Navbar />
+            <Jumbotron />
+            <Route path="/" />
+        </div>
+      </Router>
     );
   }
 }
